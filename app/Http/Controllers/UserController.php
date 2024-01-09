ab8d1e60eb650c1967fefc9d374bdcf3534e74c4
Guillermo Ricardo Garcia Garcia <86686479+RicardoGarci@users.noreply.github.com>
2024-01-09 13:41:28 -0600

    modulo usuarios
    usuarios terminado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,46 @@ class UserController extends Controller
         return view('Usuarios/CatalogoUsuarios')->with('catalogo', $catalogo);
     }
 
+    public function store (Request $request){
+
+        try{
+            $validator = Validator::make($request->all(), [
+                'txtNombre' => ['nullable', 'regex:/^[A-Za-z\s]+$/'],
+                'txtEmail' => ['nullable', 'email'],
+                'txtContrasena' => ['nullable', 'string', 'regex:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/'],
+                'txtRol' => ['nullable', 'regex:/^[A-Za-z\s]+$/']
+            ], [
+                'txtNombre.regex' => 'El campo nombre debe contener solo letras y espacios.',
+                'txtEmail.email' => 'El campo email debe ser una dirección de correo electrónico válida.',
+                'txtContrasena.regex' => 'La contraseña debe contener al menos una letra, un número y un caracter especial, y tener una longitud mínima de 8 caracteres.',
+                'txtRol.regex' => 'El campo rol debe contener solo letras y espacios.'
+            ]);
+    
+            if ($validator->fails()) {
+                Alert::info($validator->errors()->first(), null);
+                return back()->withErrors($validator)->withInput();
+            }
+
+            $catalogo = new User;
+            $catalogo->name = $request->input('txtAgregarNombre');
+            $catalogo->email = $request->input('txtAgregarEmail');
+            $catalogo->password = bcrypt($request->input('txtAgregarContrasena'));
+            $catalogo->rol = $request->input('txtAgregarRol');
+            $catalogo->deprecated = 0;
+            $catalogo->save();
+            
+            Alert::success('Usuario Agregado', 'El usuario se agrego correctamente');
+            return back();
+
+        } catch (\Exception $e) {
+             dd($e->getMessage());
+            Alert::error('Ha ocurrido un error al insertar el usuario.', null);
+            return back();
+           
+        }
+
+    }
+
     public function update(Request $request, $id)
     {
         try {
@@ -61,14 +101,16 @@ class UserController extends Controller
 
     public function destroy($id){
         try {
-            
-    
+
             $dato = User::find($id);
             $dato->deprecated = '1';
             $dato->save();
-    
             Alert::success('Usuario Eliminado', 'Los datos fueron marcados como obsoletos correctamente.');
-            return back();
+            return back();  
+
+        /*Alert::info('Confirmar eliminación', '¿Estás seguro de querer eliminar?')
+        ->showConfirmButton('Sí, eliminar', '#3085d6')
+        ->showCancelButton('No, cancelar', '#aaa'); */
         } catch (\Exception $e) {
             Alert::error('Ha ocurrido un error al marcar los datos como obsoletos.', null);
             return back();
--- resources/views/Usuarios/CatalogoUsuarios.blade.php
+++ resources/views/Usuarios/CatalogoUsuarios.blade.php
@@ -18,6 +18,13 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
+                            <div align="right">
+                                <button type="button" class="btn btn-success" data-bs-toggle="modal" 
+                                data-bs-target="#modalAgregarUsuario">
+                                    <i class=" ri-user-add-fill"></i> <span> Usuario</span>
+                                </button>
+                            </div><br>
+        
                             <div class="tab-content">
                                 <div class="tab-pane show active" id="datatable-usuarios-preview">
                                     <table id="scroll-horizontal-datatable" class="table w-100 nowrap">
@@ -47,12 +54,10 @@
                                                         method="POST" style="display: inline;">
                                                         @csrf
                                                         @method('DELETE')
-                                                        <button type="submit" class="btn btn-primary" data-bs-toggle="tooltip" 
-                                                                data-bs-placement="top" data-bs-title="Eliminar">
-                                                            <i class="ri-delete-bin-2-fill"></i>
+                                                        <button type="submit" class="btn btn-primary">
+                                                            <i class="ri-delete-bin-2-fill"></i> 
                                                         </button>
-                                        </form>
-                                                        
+                                                        </form>                
                                                     </td>   
                                                 </tr>
                                                 <!--modal editar usuarios-->
@@ -98,13 +103,54 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                            
                                                 <!--modal editar usuarios-->
                                             @endforeach
                                         </tbody>
                                     </table>
                                 </div> 
-                            </div> 
+                            </div>
+                             <!-- inicioModal de agregar  usuario-->
+                             <div id="modalAgregarUsuario" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" > Agregar Usuario </h4>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <form method="POST" action="{{ route('catalogo_usuarios.store') }}">
+                                            @csrf
+                                            <div class="modal-body">
+                                                <div class=" mb-3">
+                                                    <label >Nombre</label>
+                                                    <input type="text" class="form-control form-control-sm" name="txtAgregarNombre"  />
+                                                </div>
+                                                <div class=" mb-3">
+                                                    <label >Correo Electronico </label>
+                                                    <input type="text" class="form-control form-control-sm" name="txtAgregarEmail" />
+                                                </div>
+                                                <div class=" mb-3">
+                                                    <label >Contraseña</label>
+                                                    <div class="input-group input-group-merge">
+                                                        <input type="password" name="txtAgregarContrasena" class="form-control">
+                                                        <div class="input-group-text show-password" data-password="true">
+                                                            <span class="password-eye"></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class=" mb-3">
+                                                    <label >Rol administrativo</label>
+                                                    <input type="text" class="form-control form-control-sm" name="txtAgregarRol"/>
+                                                </div>   
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                                                <button type="submit"  class="btn btn-primary">Agregar</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                              <!-- fin  Modal de agregar usuario --> 
                         </div> 
                     </div> 
                 </div>
