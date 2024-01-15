cca4ee72d0dc4ee9e3360f6fafe56e02a5e49f35
Guillermo Ricardo Garcia Garcia <86686479+RicardoGarci@users.noreply.github.com>
2024-01-15 17:38:18 -0600

    cambios para lusillo
    git ignore se quito el vendor

--- app/Models/IntegrantesComite.php
+++ app/Models/IntegrantesComite.php
@@ -28,11 +28,11 @@ class IntegrantesComite extends Model
             ->groupBy('cm.region');
     } */
 
-    public function scopeContarPorIntegrantes($query, $ejercicio)
+   /*  public function scopeContarPorIntegrantes($query, $ejercicio)
     {
         return $query->selectRaw('
             cm.region, 
-            COUNT(DISTINCT ic.id_integrante_comite) as total, 
+            COUNT(DISTINCT ic.id_integrante_comite) as totalIntegrantes, 
             COUNT(DISTINCT CASE WHEN ic.sexo = "Mujer" THEN ic.id_integrante_comite END) as total_mujeres, 
             COUNT(DISTINCT CASE WHEN ic.sexo = "Hombre" THEN ic.id_integrante_comite END) as total_hombres,
             COUNT(DISTINCT CASE WHEN ic.lengua_indigena != "NINGUNO" THEN ic.id_integrante_comite END) as total_hablan_lengua_indigena,
@@ -40,6 +40,23 @@ class IntegrantesComite extends Model
             COUNT(DISTINCT CASE WHEN ic.sexo = "Hombre" AND NOT EXISTS (SELECT 1 FROM integrantes_comites WHERE id_acreditacion_comite_fk = ac.id_acreditacion AND sexo = "Mujer") THEN ac.id_acreditacion END) as municipios_solo_hombres,
             COUNT(DISTINCT CASE WHEN ic.sexo = "Mujer" AND EXISTS (SELECT 1 FROM integrantes_comites WHERE id_acreditacion_comite_fk = ac.id_acreditacion AND sexo = "Hombre") THEN ac.id_acreditacion END) as municipios_al_menos_una_mujer
         ')
+        ->join('acreditacion_comites as ac', 'ac.id_acreditacion', '=', 'id_acreditacion_comite_fk')
+        ->join('catalogo_municipios as cm', 'cm.id_municipio', '=', 'ac.id_catalogo_municipio_fk')
+        ->join('integrantes_comites as ic', 'ac.id_acreditacion', '=', 'ic.id_acreditacion_comite_fk')
+        ->where('ac.estatus', 4)
+        ->where('ac.ejercicio', $ejercicio)
+        ->groupBy('cm.region');
+    } */
+
+    public function scopeContarPorIntegrantes($query, $ejercicio)
+    {
+        return $query->selectRaw('
+            cm.region, 
+            COUNT(DISTINCT ic.id_integrante_comite) as totalIntegrantes, 
+            COUNT(DISTINCT CASE WHEN ic.sexo = "Mujer" THEN ic.id_integrante_comite END) as total_mujeres, 
+            COUNT(DISTINCT CASE WHEN ic.sexo = "Hombre" THEN ic.id_integrante_comite END) as total_hombres,
+            COUNT(DISTINCT CASE WHEN ic.lengua_indigena != "NINGUNO" THEN ic.id_integrante_comite END) as total_hablan_lengua_indigena
+            ')
         ->join('acreditacion_comites as ac', 'ac.id_acreditacion', '=', 'id_acreditacion_comite_fk')
         ->join('catalogo_municipios as cm', 'cm.id_municipio', '=', 'ac.id_catalogo_municipio_fk')
         ->join('integrantes_comites as ic', 'ac.id_acreditacion', '=', 'ic.id_acreditacion_comite_fk')
